6c441913a4d47308d4d049503954bbe35dc02144
NikolayK <nikolay.kim@codeimmersive.com>
2020-06-01 17:05:35 -0400

    adds loader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Drawer from './component/Drawer/Drawer';
-import Main from './component/Main/Main';
 import Navbar from './component/Navbar/Navbar';
-
+import Loader from './component/Loader/Loader'
 import { Provider } from "./component/Context/Context";
-import './App.css';
+
+const Main = React.lazy(() => import('./component/Main/Main'))
+const Drawer = React.lazy(() => import('./component/Drawer/Drawer'))
+
 
 function App() {
   return (
     <Provider>
-      <h1>CryptoKnight</h1>
-      <Drawer />
-      <Main />
-      <Navbar />
+      <React.Suspense fallback={<Loader/>}>
+        <h1>CryptoKnight</h1>
+        <Drawer />
+        <Main />
+        <Navbar />
+      </React.Suspense>
     </Provider>
   );
 }
