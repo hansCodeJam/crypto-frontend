d9a6c5685527444127395e2b1c59911c3d350c43
Hans Vos <hans.vos@codeimmersives.com>
2020-06-02 19:42:21 -0400

    npm i material ui

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Context } from '../Context/Context'
 import './Main.css'
 
+
 export default class Main extends Component {
     static contextType = Context
     render() {
