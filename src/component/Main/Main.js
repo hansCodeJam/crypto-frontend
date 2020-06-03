299be3e2a76f4378eccaf96e7952b152c7aadea8
Hans Vos <hans.vos@codeimmersives.com>
2020-06-03 12:30:22 -0400

    deleted few css files and bitcoin api working

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import { Context } from '../Context/Context'
-import './Main.css'
+import axios from 'axios';
+
 
 
 export default class Main extends Component {
     static contextType = Context
+    state = {
+        bitcoin: []
+    }
+
+    componentDidMount() {
+        axios.get('https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true').then(res => {
+            const bitcoin = res.data;
+            // console.log(bitcoin)
+            this.setState({ bitcoin })
+        })
+    }
+
+
     render() {
         return (
-            <h1>Main</h1>
+            <h1>{this.state.bitcoin.name}</h1>
         )
     }
 }
